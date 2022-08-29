a8f6d56a0cc9be21ebbb035c92ba8716128fcb06
Thibault Paulmann <thibault.paulmann@gmail.com>
2022-08-29 17:39:51 +0200

    added validations to user assignment

--- app/models/user_assignment.rb
+++ app/models/user_assignment.rb
@@ -1,4 +1,7 @@
 class UserAssignment < ApplicationRecord
+  PROGRESS = ["Not started", "Working", "Completed"]
   belongs_to :assignment
   belongs_to :user
+
+  validates :user_progress, presence: true, inclusion: { in: PROGRESS}
 end
