9d5825bf13f555bd7fb3093bda7016a0b6d605f3
Philipp Glasmacher <philipp@glasmacher.com>
2022-08-30 12:11:10 +0200

    new and create are working

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,4 +7,25 @@ class CoursesController < ApplicationController
     @course = Course.find(params[:id])
     @assignments = Assignment.where(course: @course)
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to courses_path(@course)
+      flash[:notice] = "Your listing has been created!"
+    else
+      render :new, status: :unprocessable_entity
+      flash[:alert] = "Listing creation unsuccesful"
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:code, :name)
+  end
 end
