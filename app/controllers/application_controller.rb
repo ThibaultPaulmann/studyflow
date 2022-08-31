286d96e8216f1c996b085e82f72976552f911aaa
Thibault Paulmann <thibault.paulmann@gmail.com>
2022-08-31 14:13:51 +0200

    created calendar basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_time_zone, if: :user_signed_in?
 
   # def set_current_user
   #   if session[:user_id]
   #     Current.user = User.find(id: session[:user_id])
   #   end
   # end
+
+  private
+
+  def set_time_zone
+    Time.zone = user_session[:time_zone]
+  end
 end
