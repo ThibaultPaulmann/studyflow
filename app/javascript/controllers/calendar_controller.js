e25f09975144ee2d5510a8119a76fe08e823c1d1
Thibault Paulmann <thibault.paulmann@gmail.com>
2022-09-01 12:37:34 +0200

    completed calendar

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -9,27 +9,39 @@ export default class extends Controller {
     "extensionAssignments",
   ];
 
+  previousTarget = null;
+
   connect() {
     console.log("Connected calendar controller");
   }
 
   toggleExtend(event) {
     if (this.itemTargets.includes(event.target)) {
-      console.log("extends");
       this.extend(event);
     } else {
-      console.log("shrink");
+      const calendar = document.querySelector(".simple-calendar");
+      calendar.style.borderRadius = "20px";
       this.extensionTarget.classList.add("d-none");
+      if (this.previousTarget != null) {
+        this.previousTarget.style.backgroundColor = "#1f2839";
+      }
     }
   }
 
   extend(event) {
-    console.log("adding date");
+    if (this.previousTarget != null) {
+      this.previousTarget.style.backgroundColor = "#1f2839";
+    }
+
+    event.target.style.backgroundColor = "rgba(193,207,206,0.4)";
+    this.previousTarget = event.target;
+
+    const calendar = document.querySelector(".simple-calendar");
+    calendar.style.borderRadius = "0 20px 20px 0";
+
     this.extensionDateTarget.innerText = event.target.dataset.date;
-    console.log("showing div");
     this.extensionTarget.classList.remove("d-none");
     const assignments = JSON.parse(event.target.dataset.assignments);
-    console.log(assignments);
     this.extensionAssignmentsTarget.innerHTML = "";
     assignments.forEach((assignment) => {
       this.extensionAssignmentsTarget.insertAdjacentHTML(
@@ -40,7 +52,9 @@ export default class extends Controller {
   }
 
   renderAssignment(name, time) {
-    const assignmentCard = `<div class='calendar-assignment-card d-flex justify-content-between'><p class='assignment-name'>${name}</p><p class='assignment-time'>${time}</p></div>`;
+    const reg = /T(\d\d):(\d\d)/;
+    const match = reg.exec(time);
+    const assignmentCard = `<div class='calendar-assignment-card'><p class='assignment-name'>${name}</p><p class='assignment-time'>${match[1]}:${match[2]}</p></div>`;
     return assignmentCard;
   }
 }
