66c168237dd5c5fb32c61b82f13466bbd0c2f8ab
Philipp Glasmacher <philipp@glasmacher.com>
2022-09-05 23:47:30 +0200

    implemented the logic, only gotta finish up the styling

--- app/controllers/study_sessions_controller.rb
+++ app/controllers/study_sessions_controller.rb
@@ -1,4 +1,24 @@
 class StudySessionsController < ApplicationController
+
+  def index
+    @study_sessions = StudySession.all
+  end
+
   def create
+    @study_session = StudySession.new(study_session_params)
+    @study_session.assignment = Assignment.find(params[:assignment_id])
+    if @study_session.save
+      redirect_to study_sessions_path
+      flash[:notice] = "Study Session Created!"
+    else
+      flash[:alert] = "Error creating the session"
+    end
+  end
+
+
+  private
+
+  def study_session_params
+    params.require(:study_session).permit(:title, :duration, :meeting_time)
   end
 end
--- app/models/study_session.rb
+++ app/models/study_session.rb
@@ -1,8 +1,9 @@
 class StudySession < ApplicationRecord
-  belongs_to :assignment3
+  belongs_to :assignment
   has_many :scheduled_meetings, dependent: :destroy
   has_many :users, through: :scheduled_meetings
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :meeting_time, presence: true
+  validates :duration, presence: true
 end
