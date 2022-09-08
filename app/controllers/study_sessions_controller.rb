d755f4c987fbce8b9db9122b2bdf093af1cb4cd3
Philipp Glasmacher <philipp@glasmacher.com>
2022-09-08 15:51:08 +0200

    done

--- app/controllers/study_sessions_controller.rb
+++ app/controllers/study_sessions_controller.rb
@@ -9,9 +9,10 @@ class StudySessionsController < ApplicationController
 
   def create
     @study_session = StudySession.new(study_session_params)
+    @study_session.assignment = Assignment.find(params[:assignment_id])
     if @study_session.save
-      redirect_to study_sessions_path
       flash[:notice] = "Study Session Created!"
+      redirect_to study_sessions_path
     else
       flash[:alert] = "Error creating the session"
     end
